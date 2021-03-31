a8e92065c4830165119a88bb97f9c59e6a6b3f92
Flamenrage <flamenrage@gmail.com>
2021-03-31 21:13:45 +0400

    initial , sockets + express server commit

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+    <h1>Title</h1>
+      <v-btn @click="message">New message</v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+  sockets:{
+    connect(){
+      console.log('connect method working')
+    }
+  },
+  methods:{
+    message(){
+      this.$socket.emit('createMessage', {
+        text: 'from client'
+      })
+      console.log('message method activated')
+    }
+  }
+}
+</script>
